0d8ccc7f60d51e11074be2d63d482b1af0820644
Manas Trivedi <92599846+Manas-Trivedi@users.noreply.github.com>
2026-06-13 13:02:28 +0530

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int find(int n, vector<int> &parent) {
+        int res = n;
+        while(res != parent[res]) res = parent[res];
+        return res;
+    }
+    int countComponents(int n, vector<vector<int>>& edges) {
+        int components = n;
+        vector<int> parent(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
+        vector<int> rank(n, 1);
+        for(int i = 0; i < edges.size(); i++) {
+            int p1 = find(edges[i][0], parent);
+            int p2 = find(edges[i][1], parent);
+            if(p1 == p2) continue;
+            components--;
+            if(rank[p1] > rank[p2]) {
+                parent[p2] = p1;
+            } else if(rank[p2] > rank[p1]){
+                parent[p1] = p2;
+            } else {
+                parent[p2] = p1;
+                rank[p1]++;
+            }
+        }
+        return components;
+    }
+};
